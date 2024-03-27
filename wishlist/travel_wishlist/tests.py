c08d0cf9df3bfbb1f88df03e5576a6f12a29d7be
sndmnska <102878282+sndmnska@users.noreply.github.com>
2024-03-26 23:56:41 -0500

    Add css/style, begin work on unit tests

--- wishlist/travel_wishlist/tests.py
+++ wishlist/travel_wishlist/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Place
+
+class TestHomePage(TestCase):
+
+    def test_home_page_shows_empty_list_message_for_empty_database(self):
+        home_page_url = reverse('place_list') # looks at the front end of the path 'place_list' entry in urls.py. The url is '', so this returns that url path of ''
+        response = self.client.get(home_page_url)
+        self.assertTemplateUsed('travel_wishlist/wishlist.html')
+        self.assertContains(response, "You have no places in your wishlist")
