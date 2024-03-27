f9fd2175f355c629e59ade531f55df6639c69799
sndmnska <102878282+sndmnska@users.noreply.github.com>
2024-03-27 12:37:59 -0500

    Minor code cleanup, add some comments

--- wishlist/travel_wishlist/forms.py
+++ wishlist/travel_wishlist/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Place
 
-class NewPlaceForm(forms.ModelForm):
+class NewPlaceForm(forms.ModelForm): # Creating new objects here, as far as I understand... 
     class Meta:
         model = Place
         fields = ('name', 'visited')
